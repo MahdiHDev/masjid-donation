e41855297ae97e8c05a431da328017afc50be99f
MahdiHDev <mahdi.hussain.work@gmail.com>
2026-06-06 17:01:29 +0600

    Add custom font LiAdorNoirrit and update styles in HTML and CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                bangla: ["LiAdorNoirrit", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
